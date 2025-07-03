e0d843e0873339fce22dfdab6f971187875ffcb9
Manasa G <manasa.gundra@inspirelabs.in>
2025-07-03 19:10:34 +0530

    commentedlines removed

--- routes/leaveRoutes.js
+++ routes/leaveRoutes.js
@@ -27,28 +27,7 @@ router.get(APIS.LEAVES.ENDPOINT.GET_LEAVES, async (req, res, next) => {
       .json({ message: "Failed to fetch leaves", error: error.message });
   }
 });
-// router.get(APIS.LEAVES.ENDPOINT.ADD_LEAVES, async (req, res, next) => {
-//   try {
-//     const fullUrl =
 
-//         APIS.SERVICEURLS.LEAVES +
-//         APIS.LEAVES.SERVICE_NAME +
-//         APIS.LEAVES.ENDPOINT.ADD_LEAVES;
-
-//     const response = await axios.get(fullUrl, {
-//       headers: {
-//         Authorization: req.headers["authorization"],
-//         "x-user-id": req.headers["x-user-id"],
-//       },
-//     });
-
-//     res.status(response.status).json(response.data);
-//   } catch (error) {
-//     console.error("Axios GET_LEAVES failed:", error.message);
-//     console.error("URL:", error.config?.url);
-//     res.status(500).json({ message: "Failed to fetch leaves", error: error.message });
-//   }
-// });
 router.post(APIS.LEAVES.ENDPOINT.ASSIGN_LEAVES, async (req, res, next) => {
   try {
     const response = await axios.post(
